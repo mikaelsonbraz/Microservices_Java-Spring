2583cff14fa1a23a2d78b54d3c9655d232e85aec
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-06-08 22:03:40 -0300

    FASE 1 - hr-payroll implemented

---
+++ hr-payroll/src/main/java/com/mikaelsonbraz/hrpayroll/services/PaymentService.java
@@ -0,0 +1,12 @@
+package com.mikaelsonbraz.hrpayroll.services;
+
+import com.mikaelsonbraz.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(Long workerId, Integer days){
+        return new Payment("Bob", 200.0, days);
+    }
+}
