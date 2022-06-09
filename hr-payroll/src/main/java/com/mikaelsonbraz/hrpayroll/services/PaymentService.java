6135a9cb03bda573658bf5043f4d3ccd324da77c
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-06-08 22:27:32 -0300

    FASE 1 - RestTemplate for communication tests between microservices

--- hr-payroll/src/main/java/com/mikaelsonbraz/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/mikaelsonbraz/hrpayroll/services/PaymentService.java
@@ -1,12 +1,28 @@
 package com.mikaelsonbraz.hrpayroll.services;
 
 import com.mikaelsonbraz.hrpayroll.entities.Payment;
+import com.mikaelsonbraz.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(Long workerId, Integer days){
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", workerId.toString());
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
