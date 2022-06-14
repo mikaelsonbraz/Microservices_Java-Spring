3d3cad3bd5a746b746d9f20f496d6534a83b3a9e
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-06-14 11:28:41 -0300

    FASE 1 - Ribbon load balancer implemented -> hr-payroll ribbonClient of hr-worker

--- hr-worker/src/main/java/com/mikaelsonbraz/hrworker/resources/WorkerController.java
+++ hr-worker/src/main/java/com/mikaelsonbraz/hrworker/resources/WorkerController.java
@@ -2,7 +2,10 @@ package com.mikaelsonbraz.hrworker.resources;
 
 import com.mikaelsonbraz.hrworker.entities.Worker;
 import com.mikaelsonbraz.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,11 @@ import java.util.List;
 public class WorkerController {
 
     @Autowired
+    private Environment env;
+
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    @Autowired
     private WorkerRepository workerRepository;
 
     @GetMapping
@@ -30,6 +38,7 @@ public class WorkerController {
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
         Worker worker;
         if (workerRepository.findById(id).isPresent()) {
+            logger.info("PORT = " + env.getProperty("local.server.port"));
             worker = workerRepository.findById(id).get();
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
